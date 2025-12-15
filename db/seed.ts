a00872555ce1ac3a449e4b9440787bb73f0d1a02
Carlos Santillan <csantillanj@gmail.com>
2025-12-15 13:19:04 -0600

    modified db initializer, added task points value to tasks visualization table on admin dashboard

--- db/seed.ts
+++ db/seed.ts
@@ -2,16 +2,17 @@ import { db, Users, Tasks, Rewards } from 'astro:db';
 
 export default async function seed() {
 	await db.insert(Users).values([
-		{ id: 'admin', name: 'Papá/Mamá', role: 'admin', score: 0, avatar: '👑', password: 'password123' },
+		{ id: 'mayte', name: 'Mamá', role: 'admin', score: 0, avatar: '👑', password: 'contraseña123' },
 		{ id: 'csantillanj', name: 'Papá', role: 'admin', score: 0, avatar: '👨‍💼', password: 'Adminpass.123' },
 		{ id: 'fer', name: 'Fer', role: 'child', score: 0, avatar: '🚀', password: '123' },
-		{ id: 'mateo', name: 'Mateo', role: 'child', score: 0, avatar: '🚀', password: '123' },
+		{ id: 'mateo', name: 'Mateo', role: 'child', score: 0, avatar: '🚀', password: '456' },
 	]);
 
 	await db.insert(Tasks).values([
-		{ id: 1, title: 'Tender la cama', description: 'Dejarla bien estirada', assigneeId: 'fer', status: 'pending', points: 10, dueDate: new Date(), assignedDate: new Date() },
-		{ id: 2, title: 'Lavar los platos', description: 'Después de comer', assigneeId: 'fer', status: 'completed', points: 20, dueDate: new Date(), assignedDate: new Date() },
-		{ id: 3, title: 'Hacer tarea de Mate', description: 'Páginas 20-22', assigneeId: 'mateo', status: 'pending', points: 30, dueDate: new Date(), assignedDate: new Date() },
+		{ id: 1, title: 'Limpiar la mesa', description: 'Guardar cosas, limpiar con trapo', assigneeId: 'mateo', status: 'pending', points: 30, dueDate: new Date(), assignedDate: new Date() },
+		{ id: 2, title: 'Lavar los trastes', description: 'Incluye sartenes', assigneeId: 'fer', status: 'pending', points: 20, dueDate: new Date(), assignedDate: new Date() },
+		{ id: 3, title: 'Barrer y trapear', description: 'Sala, cocina y comedor', assigneeId: 'mateo', status: 'pending', points: 30, dueDate: new Date(), assignedDate: new Date() },
+		{ id: 4, title: 'Recoger la sala', description: 'Quitar todo de los sillones', assigneeId: 'mateo', status: 'pending', points: 30, dueDate: new Date(), assignedDate: new Date() },
 	]);
 
 	await db.insert(Rewards).values([
